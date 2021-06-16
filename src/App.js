b8a16b0e17fb2a26e6f14b0d9f03cadaac19eb79
Oleh Ivanov <fenix12895@gmail.com>
2021-06-16 22:11:08 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css'
-import Header from './Header'
+import './App.css';
+import Header from './Header';
 
 const App = () => {
   return (
     <div className="test">
-      <div>123</div>
-      <Header />
-      <Header />
+      <div>123123123123123123123</div>
       <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
