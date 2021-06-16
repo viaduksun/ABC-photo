2be4409f533fb368d28783fc762f94667b801b0c
Oleh Ivanov <fenix12895@gmail.com>
2021-06-16 21:18:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+
+const App = () => {
+  return (
+    <div className="test">
+      <div>123</div>
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  )
+}
+
+export default App
