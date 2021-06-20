d3758682df9f90e9af248c4b9b240c355ff70cc7
Oleh Ivanov <fenix12895@gmail.com>
2021-06-20 17:45:34 +0300

    commit

--- src/components/UI/Button/Button.test.js
+++ src/components/UI/Button/Button.test.js
@@ -4,6 +4,6 @@ import { render } from '@testing-library/react';
 describe('Button component', () => {
   it('should render correctly', () => {
     const { getByRole } = render(<Button />);
-    getByRole('div');
+    getByRole('button');
   });
 });
