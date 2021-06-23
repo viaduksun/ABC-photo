8655ae4ce2bd19f362bf7f7a8d0f5a041303a551
Vadim Budko <viaduksun@gmail.com>
2021-06-23 14:51:55 +0300

    structure

--- src/components/Header/Dropdown/dropdownsContent.js
+++ src/components/Header/Dropdown/dropdownsContent.js
@@ -1,299 +1,180 @@
+/* eslint-disable indent */
 const dropdownsContent = [
-  {
-    categoryId: 1,
-    dropContent: [
-      {
-        title: 'Фотоаппараты',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/technics',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Видеокамеры',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/optic',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Вспышки',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/accessoryies',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Экшн камеры',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/cases',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Карты памяти',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/studio-acuipment',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '05_MenuItems'
-      },
-      {
-        title: 'Кардридеры',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/tripods',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '06_MenuItems'
-      },
-      {
-        title: 'USB флэш накопители',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/alboms',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '07_MenuItems'
-      },
-      {
-        title: 'Внешние жесткие диски',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/frames',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '08_MenuItems'
-      },
-      {
-        title: 'Фотопринтеры',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/frames',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '09_MenuItems'
-      },
-      {
-        title: 'Фотобумага для фотопринтера',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/frames',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '10_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 2,
-    dropContent: [
-      {
-        title: 'Оптика для фото',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/technics',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/optic',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/accessoryies',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/cases',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/studio-acuipment',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 3,
-    dropContent: [
-      {
-        title: 'Аксессуары',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/technics',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/optic',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/accessoryies',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/cases',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/studio-acuipment',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 4,
-    dropContent: [
-      {
-        title: 'Чехлы',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/technics',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/optic',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/accessoryies',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/cases',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/studio-acuipment',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 5,
-    dropContent: [
-      {
-        title: 'Оборудование',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/technics',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/optic',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/accessoryies',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/cases',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-<<<<<<< HEAD
-        path: '/products',
-=======
-        path: '/studio-acuipment',
->>>>>>> a8ed0603d182b38347aea24421229e989cf08e65
-        id: '05_MenuItems'
-      }
-    ]
-  },
+        {
+                categoryId: 1,
+                dropContent: [
+                        {
+                                title: 'Фотоаппараты',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Видеокамеры',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Вспышки',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Экшн камеры',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Карты памяти',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        },
+                        {
+                                title: 'Кардридеры',
+                                path: '/products',
+                                id: '06_MenuItems'
+                        },
+                        {
+                                title: 'USB флэш накопители',
+                                path: '/products',
+                                id: '07_MenuItems'
+                        },
+                        {
+                                title: 'Внешние жесткие диски',
+                                path: '/products',
+                                id: '08_MenuItems'
+                        },
+                        {
+                                title: 'Фотопринтеры',
+                                path: '/products',
+                                id: '09_MenuItems'
+                        },
+                        {
+                                title: 'Фотобумага для фотопринтера',
+                                path: '/products',
+                                id: '10_MenuItems'
+                        }
+                ],
+        },
+        {
+                categoryId: 2,
+                dropContent: [
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 3,
+                dropContent: [
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 4,
+                dropContent: [
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 5,
+                dropContent: [
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
 ];
 
 export default dropdownsContent;
