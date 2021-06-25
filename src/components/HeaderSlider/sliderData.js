a3c39344982e6ad5bc800d5365d0b6ca5ea44262
Vadim Budko <viaduksun@gmail.com>
2021-06-25 21:25:50 +0300

    slider3

--- src/components/HeaderSlider/sliderData.js
+++ src/components/HeaderSlider/sliderData.js
@@ -3,15 +3,33 @@ const sliderData = [
     id: 1,
     title: 'Скидка 10%',
     text: 'на все товары',
-    logo: '../../assets/img/logo/benro2.png',
-    image: '../../assets/img/sliderHeader/benro_01.png'
+    logo: './img/logo/benro2.png',
+    image: './img/sliderHeader/benro_01.png',
+    alt: 'benro'
   },
   {
-    id: 1,
+    id: 2,
+    title: 'Вместе дешевле',
+    text: 'фирменные чехлы',
+    logo: './img/logo/Canon.png',
+    image: './img/sliderHeader/canon_02.png',
+    alt: 'canon'
+  },
+  {
+    id: 3,
     title: 'Скидка 10%',
     text: 'на все товары',
-    logo: '../../assets/img/logo/Canon.png',
-    image: '../../assets/img/sliderHeader/canon_02.png'
+    logo: './img/logo/benro2.png',
+    image: './img/sliderHeader/benro_01.png',
+    alt: 'benro'
+  },
+  {
+    id: 4,
+    title: 'Вместе дешевле',
+    text: 'фирменные чехлы',
+    logo: './img/logo/Canon.png',
+    image: './img/sliderHeader/canon_02.png',
+    alt: 'canon'
   }
 ];
 export default sliderData;
