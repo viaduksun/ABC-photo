6b301dc945a3fbe0fc76d4465b15061b8499ec91
Vadim <74622671+viaduksun@users.noreply.github.com>
2021-07-01 22:07:52 +0300

    Oi ordering component (#38)
    * added ordering component
    
    * modified ordering component
    
    Co-authored-by: Oleg Ivanov <u014@Altera2.local>

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,10 +5,7 @@ import CheckoutBlock from '../components/CheckoutBlock/CheckoutBlock';
 const Checkout = () => (
   <>
     <Breadcrumbs />
-    <div className="container">
-      <h1>Checkout page</h1>
-      <CheckoutBlock />
-    </div>
+    <CheckoutBlock />
   </>
 );
 
