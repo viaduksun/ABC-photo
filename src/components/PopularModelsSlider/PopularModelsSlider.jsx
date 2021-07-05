a54786f86fe95fdba348c0fdae2edc7bc9216640
Vadim <74622671+viaduksun@users.noreply.github.com>
2021-07-05 20:29:20 +0300

    fix-product-card (#43)
    Co-authored-by: Oleg Ivanov <u014@Altera2.local>

--- src/components/PopularModelsSlider/PopularModelsSlider.jsx
+++ src/components/PopularModelsSlider/PopularModelsSlider.jsx
@@ -1,48 +1,46 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Pagination, Navigation } from 'swiper/core';
-import styles from './PopularModelsSlider.module.scss';
-import ProductCard from '../ProductCard/ProductCard';
-
 import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
 import 'swiper/components/navigation/navigation.min.css';
+import SwiperCore, { Pagination, Navigation, Autoplay } from 'swiper/core';
+import ProductCard from '../ProductCard/ProductCard';
 import './PopularModelsSlider.scss';
+import styles from './PopularModelsSlider.module.scss';
 
-SwiperCore.use([Pagination, Navigation]);
+SwiperCore.use([Pagination, Navigation, Autoplay]);
 
-const PopularModelsSlider = () => (
-  <div className={styles.PopularContainer}>
-    <h1 className={styles.SliderHeader}>Популярные модели</h1>
-    <div className={styles.PopularModelsSlider}>
-      <Swiper
-        className="swiper-container-3"
-        slidesPerView={4}
-        // spaceBetween={10}
-        slidesPerGroup={1}
-        loop
-        loopFillGroupWithBlank
-        pagination={false}
-        navigation
-        autoplay
-      >
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <ProductCard />
-        </SwiperSlide>
-      </Swiper>
+const SwiperCarousel = () => (
+  <>
+    <div className={styles.PopularContainer}>
+      <h1 className={styles.SliderHeader}>Популярные модели</h1>
+      <div className={styles.PopularModelsSlider}>
+        <Swiper
+          className="swiper-container-3"
+          slidesPerView={4}
+          spaceBetween={55}
+          autoplay={{ delay: 3000 }}
+          slidesPerGroup={1}
+          loop
+          loopFillGroupWithBlank
+          navigation
+        >
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
-  </div>
+  </>
 );
 
-export default PopularModelsSlider;
+export default SwiperCarousel;
