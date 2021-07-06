e8cfeed7bfacb245467c75498268ada9d156275c
Vadim <74622671+viaduksun@users.noreply.github.com>
2021-07-06 16:06:36 +0300

    Vb profile (#49)
    * tabs
    
    * field
    
    * form
    
    * profile

--- src/components/ProfileBlock/ProfileBlock.jsx
+++ src/components/ProfileBlock/ProfileBlock.jsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import EditProfile from './EditProfile';
+import Orders from './Orders';
+import ChangePassword from './ChangePassword';
 import styles from './ProfileBlock.module.scss';
 
-const ProfileBlock = () => (
-  <div className={styles.ProfileBlock}>
-    <h1>My profile block</h1>
-    <div className={styles.btnBox}>
-      <Link to="/">
-        <button type="button" className="btn">
-          Готово
-        </button>
-      </Link>
+const ProfileBlock = () => {
+  const [current, setCurrent] = useState('edit');
+  const handleEdit = () => {
+    setCurrent('edit');
+  };
+  const handleChange = () => {
+    setCurrent('change');
+  };
+  const handleOrders = () => {
+    setCurrent('orders');
+  };
+  const btnEdit = classNames({
+    [styles.btnProfile]: true,
+    [styles.btnProfile_active]: current === 'edit',
+  });
+  const btnChange = classNames({
+    [styles.btnProfile]: true,
+    [styles.btnProfile_active]: current === 'change',
+  });
+  const btnOrders = classNames({
+    [styles.btnProfile]: true,
+    [styles.btnProfile_active]: current === 'orders',
+  });
+  return (
+    <div className={styles.ProfileBlock}>
+      <div className={styles.header}>Личный кабинет</div>
+      <div className={styles.content}>
+        <div className={styles.btnBlock}>
+          <button className={btnEdit} type="button" onClick={handleEdit}>
+            Редактировать профиль
+          </button>
+          <button className={btnChange} type="button" onClick={handleChange}>
+            Изменить пароль
+          </button>
+          <button className={btnOrders} type="button" onClick={handleOrders}>
+            Мои заказы
+          </button>
+
+          <Link to="/">
+            <button className={styles.btnProfile} type="button">
+              Выйти из кабинета
+            </button>
+          </Link>
+        </div>
+        <div className={styles.optionContent}>
+          {current === 'edit' && <EditProfile />}
+          {current === 'change' && <ChangePassword />}
+          {current === 'orders' && <Orders />}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfileBlock;
